a5ba0db792bab1a84b001646076fdb5b680d2257
z631200 <92363461+z631200@users.noreply.github.com>
2022-04-13 17:06:54 +0800

    Create Selling.java

---
+++ 0413/Selling.java
@@ -0,0 +1,10 @@
+public class Selling{
+    public static void main(String [] args){
+
+        Ticket t1 = new Ticket("2021/4/29", 250);
+        Ticket t2 = new Ticket("2021/4/29", "Johnny Chen");        
+
+        System.out.println(t1);
+        System.out.println(t2);
+    }
+}
